a743f41b606696a5fa7194041c8b385ffff7c7d0
GomidesTs <gomidestulio5@gmail.com>
2022-10-26 21:36:35 -0300

    :tada: feat: initial project structure - Institutional page for the NGO Sociedade to grow from developed in React

---
+++ src/styles/theme.js
@@ -0,0 +1,13 @@
+export default {
+    COLORS:{
+        BACKGROUND_900: '#202628',
+        BACKGROUND_500: '#9F9A9A',
+        BACKGROUND_100: '#D3DADC',
+
+        WHITE: '#FFFFFF',
+        BLACK: '#000000',
+
+        TEXT_BLUE: '#087D96',
+        TEXT_ORANGE: '#D14200',
+    }
+}
