8fc56d2d56a0155c19e9f66833381f92ee868200
GomidesTs <gomidestulio5@gmail.com>
2022-10-27 02:10:56 -0300

    :sparkles: feat: structure - Implementing global styling - Rendering routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 
 import { ThemeProvider } from 'styled-components'
+import GlobalStyle from './styles/global'
 
 import theme from './styles/theme'
 
+import { Routes } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <GlobalStyle />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>
 )
